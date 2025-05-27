946f6d2c5bbe696fe060c5efc27f46499d2545ae
Marco-Rocco <marcorocco28@icloud.com>
2025-05-27 18:00:09 +0200

    feat: change jumbotron and background + create mediaquery for jumbotron

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 const Header = () => {
     return (
         <>
-            <header className="bg-success-subtle py-3 mb-2 position-fixed shadow w-100 z-3">
+            <header className=" py-3 mb-2 position-fixed shadow w-100 z-3">
                 <div className="container d-flex justify-content-between align-items-center">
                     <div>Sergente Serpente</div>
                     <ul className="d-flex list-unstyled m-o gap-2">
