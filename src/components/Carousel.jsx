1f3f39612f8df2c05ea84cc7a11abefbe9c24f41
Marco-Rocco <marcorocco28@icloud.com>
2025-05-27 18:35:45 +0200

    feat: create carousel

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,30 @@
+export default function Carousel() {
+    return (
+        <>
+            <div id="carouselExampleSlidesOnly" className="carousel slide fs-5" data-bs-ride="carousel">
+                <div className="carousel-inner">
+                    <div className="carousel-item active">
+                        <img src="/istockphoto-157479804-612x612.jpg" className="d-block w-100" alt="..." />
+                    </div>
+
+                    <div className="carousel-item">
+                        <img src="/istockphoto-487506120-612x612.jpg" className="d-block w-100" alt="..." />
+                    </div>
+
+                    <div className="carousel-item">
+                        <img src="/istockphoto-1304421737-612x612.jpg" className="d-block w-100" alt="..." />
+                    </div>
+
+                    <div className="carousel-item">
+                        <img src="/istockphoto-1298772937-612x612.jpg" className="d-block w-100" alt="..." />
+                    </div>
+
+                    <div className="carousel-item">
+                        <img src="/serpentecorallo.avif" className="d-block w-100" alt="..." />
+                    </div>
+
+                </div>
+            </div>
+        </>
+    )
+}
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,10 +1,13 @@
+import Carousel from "./Carousel";
 import Jumbotron from "./Jumbotron";
 
 export default function HomePage() {
     return (
         <>
             <Jumbotron />
-            <h1 className="text-center text-light">BENVENUTO SU SERGENTE SERPENTE</h1>
+
+            <Carousel />
+
         </>
     )
 };
