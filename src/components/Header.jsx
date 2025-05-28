f74efd99391a54bce915b1921232c61d2f735131
Marco-Rocco <marcorocco28@icloud.com>
2025-05-28 16:42:26 +0200

    fix: fix button to go back to previous page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,46 +1,48 @@
 import { NavLink, useLocation, useNavigate } from "react-router-dom";
 
 const Header = () => {
-  const location = useLocation();
-  const navigate = useNavigate();
+    const location = useLocation();
+    const navigate = useNavigate();
 
-  // Mostra il bottone solo se NON siamo sulla home
-  const showBackHome = location.pathname !== "/";
+    // Mostra il bottone solo se NON siamo sulla home
+    const goBack = () => {
+        navigate(-1);
+    };
 
-  return (
-    <>
-      <header className="py-3 shadow w-100 z-3">
-        <div className="container">
-          <ul className="d-flex list-unstyled m-0 justify-content-between align-items-center mx-5 fs-2 fw-bold">
-            <li className="logo-container">
-              <NavLink to="/" className="text-decoration-none">
-                <img src="/logo-sergente-serpente.jpg" alt="Logo" className="img-logo me-3" />
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/snakes" className="text-decoration-none">shop</NavLink>
-            </li>
-            <li>
-              <NavLink to="/blog" className="text-decoration-none">blog</NavLink>
-            </li>
-            <li>
-              <NavLink to="/cart" className="text-decoration-none">
-                <i className="fa-solid fa-bag-shopping"></i>
-              </NavLink>
-            </li>
-          </ul>
-        </div>
+    return (
+        <>
+            <header className="py-3 shadow w-100 z-3">
+                <div className="container">
+                    <ul className="d-flex list-unstyled m-0 justify-content-between align-items-center mx-5 fs-2 fw-bold">
+                        <li className="logo-container">
+                            <NavLink to="/" className="text-decoration-none">
+                                <img src="/logo-sergente-serpente.jpg" alt="Logo" className="img-logo me-3" />
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/snakes" className="text-decoration-none">shop</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/blog" className="text-decoration-none">blog</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/cart" className="text-decoration-none">
+                                <i className="fa-solid fa-bag-shopping"></i>
+                            </NavLink>
+                        </li>
+                    </ul>
+                </div>
 
-        {showBackHome && (
-          <div className="container mt-2">
-            <button className="btn btn-outline-secondary" onClick={() => navigate("/")}>
-              Torna alla Home
-            </button>
-          </div>
-        )}
-      </header>
-    </>
-  );
+                {goBack && (
+                    <div className="container mt-2">
+                        <button className="btn btn-outline-secondary" onClick={goBack}>
+                            indietro
+                        </button>
+                    </div>
+                )}
+            </header>
+        </>
+    );
 };
 
 export default Header;
