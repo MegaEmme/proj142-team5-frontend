97be7ce2a6d9318eeef6759c3a73d1c588225c99
BassaniDavid <davbassan98@gmail.com>
2025-05-28 10:45:02 +0200

    feat: update Header component with improved navigation and styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,23 +4,22 @@ const Header = () => {
     return (
         <>
             <header className="bg-success-subtle py-3 mb-2 position-fixed shadow w-100 z-3">
-                <div className="container d-flex justify-content-between align-items-center">
-                    <div>Sergente Serpente</div>
-                    <ul className="d-flex list-unstyled m-o gap-2">
+                <div className="container">
+                    <ul className="d-flex list-unstyled m-o justify-content-between align-items-center mx-5 fs-4 fw-bold">
                         <li>
-                            <NavLink to='/'>Home Page</NavLink>
+                            <NavLink to='/' className="text-decoration-none">Sergente Serpente</NavLink>
                         </li>
                         <li>
-                            <NavLink to='/api/snakes'>Serpenti</NavLink>
+                            <NavLink to='/api/snakes' className="text-decoration-none">Serpenti</NavLink>
                         </li>
                         <li>
-                            <NavLink to='/api/snakes/:slug'>Serpente</NavLink>
+                            <NavLink to='/api/snakes/:slug' className="text-decoration-none">Serpente</NavLink>
                         </li>
                         <li>
-                            <NavLink to='/api/blog'>blog</NavLink>
+                            <NavLink to='/api/blog' className="text-decoration-none">blog</NavLink>
                         </li>
                         <li>
-                            <NavLink to='/api/cart'>carrello</NavLink>
+                            <NavLink to='/api/cart' className="text-decoration-none"><i class="fa-solid fa-bag-shopping"></i></NavLink>
                         </li>
                     </ul>
                 </div>
