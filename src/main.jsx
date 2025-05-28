702d5ec0cf24fe9a55de5fde6517da2b59b76ce2
Fabio-Bianco <biancobfabio@gmail.com>
2025-05-28 10:41:40 +0200

    feat: add Bootstrap for styling and enhance SnakesPage with sorting and filtering options

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js' // IMPORTANTE per far funzionare il menu
 import './index.css'
 import App from './App.jsx'
 
