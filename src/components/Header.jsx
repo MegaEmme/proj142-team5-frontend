63a8d1ab5b5985b85c30f565f2bfd21314f701c4
BassaniDavid <davbassan98@gmail.com>
2025-05-28 13:46:31 +0200

    fix: update header styles and correct navigation link text

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,12 +5,12 @@ const Header = () => {
         <>
             <header className="bg-success-subtle py-3 mb-2 position-fixed shadow w-100 z-3">
                 <div className="container">
-                    <ul className="d-flex list-unstyled m-o justify-content-between align-items-center mx-5 fs-4 fw-bold">
+                    <ul className="d-flex list-unstyled m-o justify-content-between align-items-center mx-5 fs-2 fw-bold">
                         <li className="logo-container">
                             <NavLink to='/' className="text-decoration-none"><img src="/logo-sergente-serpente.jpg" alt="" className="img-logo me-3" /></NavLink>
                         </li>
                         <li>
-                            <NavLink to='/api/snakes' className="text-decoration-none">Serpenti</NavLink>
+                            <NavLink to='/api/snakes' className="text-decoration-none">shop</NavLink>
                         </li>
                         <li>
                             <NavLink to='/api/snakes/:slug' className="text-decoration-none">Serpente</NavLink>
