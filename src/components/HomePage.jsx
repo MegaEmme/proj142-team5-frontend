61871f5ff9737d0cfe9619ed6ae7af2661cf8b92
BassaniDavid <davbassan98@gmail.com>
2025-05-29 13:30:15 +0200

    feat: update HomePage layout and add new sections; improve blog post texts

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -7,16 +7,27 @@ export default function HomePage() {
         <>
             <Jumbotron />
 
-            <div class="card text-center mb-3 blogcard w-75">
-                <div class="card-body">
-                    <h5 class="card-title">Benvenuto su <strong>Sergente Serpente!</strong> </h5>
-                    <p class="card-text">Sei nel posto giusto per scoprire il fascino ineguagliabile di questi magnifici rettili. Qui a Sergente Serpente, ti offriamo una selezione curata di serpenti, allevati con passione e dedizione per garantirti esemplari sani, robusti e pronti ad arricchire la tua vita. Esplora la nostra collezione e trova il compagno squamato perfetto per te!</p>
-                    <a href="/snakes" class="btn btnblog">Dai un'occhiata ai nostri esemplari</a>
+            <div className="card text-center mb-3 blogcard w-75">
+                <div className="card-body">
+                    <h5 className="card-title">Benvenuto su <strong>Sergente Serpente!</strong> </h5>
+                    <p className="card-text">Sei nel posto giusto per scoprire il fascino ineguagliabile di questi magnifici rettili. Qui a Sergente Serpente, ti offriamo una selezione curata di serpenti, allevati con passione e dedizione per garantirti esemplari sani, robusti e pronti ad arricchire la tua vita. Esplora la nostra collezione e trova il compagno squamato perfetto per te!</p>
+                    <a href="/snakes" className="btn btnblog">Dai un'occhiata ai nostri esemplari</a>
                 </div>
             </div>
 
             <Carousel />
 
+            <div className="card container w-75 mb-3 py-2 bg-danger">
+                sezione 1 con i serpenti recenti da aggiungere!
+            </div>
+
+            <div className="card container w-75 mb-3 py-2 bg-danger">
+                sezione 1 con i serpenti in sconto da aggiungere!
+            </div>
+
+            <div className="card container w-75 mb-3 py-2">
+                <h2>scopri interessanti funfact sui serpenti!</h2>
+            </div>
             <FunFactSection />
 
         </>
