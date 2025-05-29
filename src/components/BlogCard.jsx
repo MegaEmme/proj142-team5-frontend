36789e5e8082c452f6c24d0295dd3dd7c64093d8
BassaniDavid <davbassan98@gmail.com>
2025-05-29 10:04:23 +0200

    feat: enhance BlogCard layout and add hover effects for price and button

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -3,17 +3,17 @@ import { Link } from "react-router-dom";
 const BlogCard = ({ blog }) => {
 
     return (
-        <div className=" mb-5 p-3 card blogcard"> {/* Colonne Bootstrap per layout responsive */}
-            <div className="h-100 shadow-sm d-flex flex-row"> {/* Card Bootstrap con altezza fissa e ombra */}
+        <div className=" mb-5 p-3 card blogcard shadow-sm"> {/* Colonne Bootstrap per layout responsive */}
+            <div className="h-100 d-flex flex-row">
                 <img
                     src={blog.image}
-                    className="img-fluid w-25 m-4" // img-fluid per responsività
+                    className="img-fluid w-25 m-4 rounded-3" // img-fluid per responsività
                     alt={blog.title}
                 />
-                <div className="card-body d-flex flex-column">
-                    <h2 className="card-title text-truncate my-2">{blog.title}</h2> {/* text-truncate se il titolo è lungo */}
-                    <h4 className="card-subtitle mb-2 my-2">{blog.category}</h4>
-                    <h5 className="card-text flex-grow-1 my-2">{blog.excerpt}</h5> {/* flex-grow-1 per occupare spazio */}
+                <div className=" d-flex flex-column">
+                    <h2 className=" text-truncate my-2">{blog.title}</h2> {/* text-truncate se il titolo è lungo */}
+                    <h4 className=" mb-2 my-2">{blog.category}</h4>
+                    <h5 className=" flex-grow-1 my-2">{blog.excerpt}</h5> {/* flex-grow-1 per occupare spazio */}
                     <div className="d-flex justify-content-between align-items-center my-2 px-5">
                         <small>di {blog.author}</small>
                         <small>{blog.date}</small>
