dcca254afcdac2a3ac0df0cfdc3022571317aa4c
BassaniDavid <davbassan98@gmail.com>
2025-05-29 10:48:10 +0200

    feat: add floating image style to BlogPageDetails and enhance layout

--- src/pages/BlogPageDetails.jsx
+++ src/pages/BlogPageDetails.jsx
@@ -14,15 +14,18 @@ export default function BlogPageDetails() {
         date
     } = blogs[id - 1]
 
-
     return (
         <div className="card p-3 my-5 blogcard">
-            <img src={image} alt={title} />
-            <h1>{title}</h1>
-            <h2>categoria: {category}</h2>
-            <p>{text}</p>
-            <p>{author}</p>
-            <p>{date}</p>
+
+            <p><img src={image} alt={title} className="imgfloat m-4" />
+                <span className="fs-1">{title}</span><br />
+                <span className="fs-2">categoria: {category}</span><br />
+                <span>{text}</span><br />
+            </p>
+            <div className="d-flex justify-content-between mt-3">
+                <p>{author}</p>
+                <p>{date}</p>
+            </div>
         </div>
     )
 }
