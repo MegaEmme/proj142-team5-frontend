1dfd1eb0ecc6f56c673490449c7fc378d2ee2658
Marco-Rocco <marcorocco28@icloud.com>
2025-05-29 18:28:19 +0200

    feat: add image

--- src/components/SnakeDetailCard.jsx
+++ src/components/SnakeDetailCard.jsx
@@ -18,7 +18,7 @@ const SnakeDetailCard = ({ data }) => {
 
         <>
             <div className="card mb-3">
-                <img src={`./snake-imgs/${image}`} className="card-img-top" alt={scientific_name} />
+                <img src={`../snake-imgs/${image}`} className="card-img-top" alt={scientific_name} />
                 <div className="card-body">
                     <h5 className="card-title">{scientific_name} </h5>
                     <p className="card-text">il {scientific_name}, conosciuto come {common_name}, è un {description}</p>
