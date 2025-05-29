6f915b6eba5ad7e3cfcc589f6645acbd31f91dcc
BassaniDavid <davbassan98@gmail.com>
2025-05-29 13:40:17 +0200

    feat: update Carousel component to display blog posts with dynamic content

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,93 +1,97 @@
+import blogs from "../data/blogPosts";
+
 export default function Carousel() {
-  return (
-    <div className="d-flex justify-content-center align-items-center my-5">
-      <div
-        id="carouselExampleCaptions"
-        className="carousel slide w-75"
-        data-bs-ride="carousel"
-        data-bs-interval="3000"
-      >
-        <div className="carousel-indicators">
-          <button
-            type="button"
-            data-bs-target="#carouselExampleCaptions"
-            data-bs-slide-to="0"
-            className="active"
-            aria-current="true"
-            aria-label="Slide 1"
-          ></button>
-          <button
-            type="button"
-            data-bs-target="#carouselExampleCaptions"
-            data-bs-slide-to="1"
-            aria-label="Slide 2"
-          ></button>
-          <button
-            type="button"
-            data-bs-target="#carouselExampleCaptions"
-            data-bs-slide-to="2"
-            aria-label="Slide 3"
-          ></button>
+
+
+    return (
+        <div className="d-flex justify-content-center align-items-center my-5">
+            <div
+                id="carouselExampleCaptions"
+                className="carousel slide w-75"
+                data-bs-ride="carousel"
+                data-bs-interval="3000"
+            >
+                <div className="carousel-indicators">
+                    <button
+                        type="button"
+                        data-bs-target="#carouselExampleCaptions"
+                        data-bs-slide-to="0"
+                        className="active"
+                        aria-current="true"
+                        aria-label="Slide 1"
+                    ></button>
+                    <button
+                        type="button"
+                        data-bs-target="#carouselExampleCaptions"
+                        data-bs-slide-to="1"
+                        aria-label="Slide 2"
+                    ></button>
+                    <button
+                        type="button"
+                        data-bs-target="#carouselExampleCaptions"
+                        data-bs-slide-to="2"
+                        aria-label="Slide 3"
+                    ></button>
+                </div>
+
+                <div className="carousel-inner">
+                    <div className="carousel-item active">
+                        <img
+                            src={blogs[0].image}
+                            className="d-block w-100"
+                            alt={blogs[0].title}
+                        />
+                        <div className="carousel-caption d-none d-md-block bg-dark">
+                            <h5>{blogs[0].title}</h5>
+                            <p>{blogs[0].excerpt}</p>
+                        </div>
+                    </div>
+
+                    <div className="carousel-item">
+                        <img
+                            src={blogs[1].image}
+                            className="d-block w-100"
+                            alt={blogs[1].title}
+                        />
+                        <div className="carousel-caption d-none d-md-block bg-dark">
+                            <h5>{blogs[1].title}</h5>
+                            <p>{blogs[1].excerpt}</p>
+                        </div>
+                    </div>
+
+                    <div className="carousel-item">
+                        <img
+                            src={blogs[2].image}
+                            className="d-block w-100"
+                            alt={blogs[2].title}
+                        />
+                        <div className="carousel-caption d-none d-md-block bg-dark">
+                            <h5>{blogs[2].title}</h5>
+                            <p>{blogs[2].excerpt}</p>
+                        </div>
+                    </div>
+                </div>
+
+                <button
+                    className="carousel-control-prev"
+                    type="button"
+                    data-bs-target="#carouselExampleCaptions"
+                    data-bs-slide="prev"
+                >
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Previous</span>
+                </button>
+
+                <button
+                    className="carousel-control-next"
+                    type="button"
+                    data-bs-target="#carouselExampleCaptions"
+                    data-bs-slide="next"
+                >
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Next</span>
+                </button>
+            </div>
         </div>
-
-        <div className="carousel-inner">
-          <div className="carousel-item active">
-            <img
-              src="/istockphoto-157479804-612x612.jpg"
-              className="d-block w-100"
-              alt="..."
-            />
-            <div className="carousel-caption d-none d-md-block">
-              <h5>First slide label</h5>
-              <p>Some representative placeholder content for the first slide.</p>
-            </div>
-          </div>
-
-          <div className="carousel-item">
-            <img
-              src="/istockphoto-157479804-612x612.jpg"
-              className="d-block w-100"
-              alt="..."
-            />
-            <div className="carousel-caption d-none d-md-block">
-              <h5>Second slide label</h5>
-              <p>Some representative placeholder content for the second slide.</p>
-            </div>
-          </div>
-
-          <div className="carousel-item">
-            <img
-              src="/istockphoto-157479804-612x612.jpg"
-              className="d-block w-100"
-              alt="..."
-            />
-            <div className="carousel-caption d-none d-md-block">
-              <h5>Third slide label</h5>
-              <p>Some representative placeholder content for the third slide.</p>
-            </div>
-          </div>
-        </div>
-
-        <button
-          className="carousel-control-prev"
-          type="button"
-          data-bs-target="#carouselExampleCaptions"
-          data-bs-slide="prev"
-        >
-          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span className="visually-hidden">Previous</span>
-        </button>
-
-        <button
-          className="carousel-control-next"
-          type="button"
-          data-bs-target="#carouselExampleCaptions"
-          data-bs-slide="next"
-        >
-          <span className="carousel-control-next-icon" aria-hidden="true"></span>
-          <span className="visually-hidden">Next</span>
-        </button>
-      </div>
-    </div>
-  );
+    );
 }
