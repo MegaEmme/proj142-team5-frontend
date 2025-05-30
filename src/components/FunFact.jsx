81713f0e12a4f1815834430a839f644c32c4fca4
BassaniDavid <davbassan98@gmail.com>
2025-05-30 10:27:25 +0200

    refactor: enhance FunFactSection styling and update CSS for funfactcard-mini

--- src/components/FunFact.jsx
+++ src/components/FunFact.jsx
@@ -2,17 +2,17 @@ import funFacts from "../data/funFacts";
 
 export default function FunFactSection() {
     return (
-        <div className="container my-5">
+        <div className="container my-5 ">
             <h2 className="text-center text-white mb-4">Curiosità dal mondo dei serpenti</h2>
-            <div className="row justify-content-center">
+            <div className="row justify-content-center ">
                 {funFacts.length ? (
                     funFacts.map(fact => (
-                        <div className="col-12 col-md-4 mb-4" key={fact.id}>
-                            <div className="card funfactcard-mini h-100 shadow-sm p-2 d-flex flex-column align-items-center">
+                        <div className="col-12 col-md-4 mb-4 " key={fact.id}>
+                            <div className="card funfactcard-mini blogcard h-100 shadow-sm p-2 d-flex flex-column align-items-center">
                                 <img src={fact.image} className="card-img-top rounded-3 img-funfact" alt={fact.titolo} />
-                                <div className="card-body p-2 text-center">
-                                    <h6 className="card-title">{fact.titolo}</h6>
-                                    <p className="card-text small">{fact.contenuto}</p>
+                                <div className=" p-2 text-center">
+                                    <h6>{fact.titolo}</h6>
+                                    <p className="small">{fact.contenuto}</p>
                                     <button className="btn btnblog mt-2 btn-sm">Scopri di più</button>
                                 </div>
                             </div>
