2f3f412ddc1fcd901fbcadde626369e760bb0da4
MegaEmme <mikmusco@gmail.com>
2025-06-04 13:27:29 +0200

    fix: correct wording in NotFound component message

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -2,7 +2,7 @@ const NotFound = () => {
     return (
         <div className="container vh-100 d-flex justify-content-center align-items-center">
             <div className="bg-danger p-4 rounded text-center text-white w-100">
-                <h2>Ops... qui non ci sono serpenti...</h2>
+                <h2>Ops... qui non sembrano esserci serpenti...</h2>
             </div>
         </div>
     );
